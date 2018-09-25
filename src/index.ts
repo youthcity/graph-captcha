a85adecb69d28b96c7d9d054cec73790bb0093e6
tiancity <wutiancity@163.com>
2018-09-25 23:26:44 +0800

    chore(*): add koa and static files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import * as bodyParser from 'koa-bodyparser';
+import * as serve from 'koa-static';
 
-const main = () => {
-  console.log('===');
-};
+import path = require('path');
 
-main();
+const PORT = 4000;
+
+const app = new Koa();
+const router = new Router();
+
+const staticPath = '../static';
+
+app.use(bodyParser());
+app.use(serve(
+  path.join(__dirname, staticPath),
+));
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'hello world';
+});
+
+app.use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(PORT);
+console.log(`app started at  http://localhost:${PORT} `);
